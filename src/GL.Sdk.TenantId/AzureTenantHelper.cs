1f811f6cddd031c77dec8e15285b65e71c45558d
Giancarlo Lelli <gcarlo.lelli@live.com>
2019-02-19 14:41:46 +0100

    Initial commit

---
+++ src/GL.Sdk.TenantId/AzureTenantHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GL.Sdk.TenantId
+{
+    public class AzureTenantHelper
+    {
+        private readonly HttpClient m_client;
+        private readonly ILogger m_logger;
+
+        public AzureTenantHelper(HttpClient client, ILogger logger)
+        {
+            m_client = client ?? throw new ArgumentNullException(nameof(client));
+            m_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public AzureTenantHelper(HttpClient client)
+        {
+            m_client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<Guid> ResolveId(string tenantName)
+        {
+            var tenantId = Guid.Empty;
+
+            try
+            {
+                var url = $"https://login.windows.net/{tenantName}/.well-known/openid-configuration";
+                var jsonString = await m_client.GetStringAsync(url);
+                var jObject = JObject.Parse(jsonString);
+
+                if (jObject.TryGetValue("issuer", out JToken issuer))
+                {
+                    var issuerTokenValue = issuer.ToString();
+                    var split = issuerTokenValue.Split('/');
+
+                    var textId = split[3];
+                    tenantId = Guid.Parse(textId);
+                }
+            }
+            catch (Exception ex)
+            {
+                m_logger?.LogError(ex.Message, ex);
+            }
+
+            return tenantId;
+        }
+    }
+}
---
+++ src/GL.TenantID/Program.cs
@@ -0,0 +1,18 @@
+﻿using GL.Sdk.TenantId;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GL.TenantID
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            var helper = new AzureTenantHelper(new HttpClient(), null);
+            var id = await helper.ResolveId("mspbipro.onmicrosoft.com");
+        }
+    }
+}
